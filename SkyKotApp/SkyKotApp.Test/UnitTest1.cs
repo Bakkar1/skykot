24c5c0b52a45028dc6090fa5dc16776be377a5e0
M'Bark Bakkar <mbarkbakkar1@gmail.com>
2022-07-17 12:14:42 +0200

    fix navbar

---
+++ SkyKotApp/SkyKotApp.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Moq;
+using SkyKotApp.Services.General;
+
+namespace SkyKotApp.Test;
+
+public class UnitTest1
+{
+    public readonly Mock<ISkyKotRepository> _dbServiceMock = new();
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
